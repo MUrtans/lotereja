8174ca311f79b135a120eb389503a63443c42c99
MUrtans <martins.urtans1710@gmail.com>
2022-01-20 15:00:09 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1);
+const vardi=['Jana Krūmiņa','Jānis Bērziņš','Dace Priede','Arturs Vanags','Rihards Ozoliņš'];
+const balvas=['Xbox 360','labs ledusskapis','iphone iks','Honda Jazz','kvalitatīva datorpele'];
+const naudaKopa=100000;//kopeja summma
+let uzvaretjuSkaits=5;
+let rand= Math.random()*5;
+rand= Math.floor(rand);//noappalo uz leju
+console.log(vardi[rand]);//izvada konsole
